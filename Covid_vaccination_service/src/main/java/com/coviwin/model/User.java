aea45ac04aa3813069ad4971bb870360bbf7633f
Kundan-10 <kundanwakudkar10@gmail.com>
2025-01-12 13:54:12 +0530

    update dependency

--- Covid_vaccination_service/src/main/java/com/coviwin/model/User.java
+++ Covid_vaccination_service/src/main/java/com/coviwin/model/User.java
@@ -5,16 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.UniqueElements;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
